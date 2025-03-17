19a72f5d2a0a733e9a4ba4abd0bf9c125b7ef079
DawnProspect <rzaljabar@gmail.com>
2025-03-17 17:45:56 +0700

    finish learning data type

--- javascript/data-types-and-conditionals/datanote.js
+++ javascript/data-types-and-conditionals/datanote.js
@@ -1,5 +1,83 @@
 // * 8 Tipe data di JavaScript
 
+/* 
+
+OVERVIEW
+
+NUMBER
+
+BIGINT
+
+STRING
+
+BOOLEAN
+
+NULL VALUE
+
+UNDEFINED VALUE
+
+OBJECTS AND SYMBOLS
+
+TYPEOF OPERATOR
+
+
+Setiap value di javascript pasti memiliki tipe contohyna string/number, maka dari itu kita coba pelajari tipe data data ini
+*/
+
+// ! PEMISAH
+
+// * NUMBER
+/* 
+
+Number type bisa representasi integer dan floating point numbers
+
+misalnya
+
+let n = 123
+
+n = 12.345
+
+Lalu banyak operations untuk angka juga seperti multiplication *, division /, addition +, subtraction -, 
+
+Di tipe data ini juga ada namanya special numeric values yaitu Infinity, -Infinity, dan NaN
+
+- Infinity: merepresentasikan infinity dari matematika yang memiliki value lebih besar dari angka berapapun
+
+misalnya:
+
+alert( 1 / 0 ) hasilnya Infinity
+
+atau
+
+alert( Infinity ) hasilnya juga sama Infinity
+
+- NaN: NaN itu tanda bahwa ada computational error, karena hasil suatu perhitungan yang salah atau undefined
+
+misalnya:
+
+alert ( "Not a number" / 2 ) pasti hasilnya NaN karena membagi string dengan angka akan membuat komputer bingung
+
+*/
+
+// ! PEMISAH
+
+// *BigInt
+
+/* 
+
+Tipe data bisa menyimpan integer yang besar sampai 1.7976931348623157 * 10308 tetapi diluar integer yang aman ±(253-1) akan ada error, karena tidak semua digit bisa disimpan
+
+Kenapa tipe data ini diperlukan? biasanya butuh buat cryptography atau timestamp yang sampai menghitung microsecond
+
+Tipe data BigInt biasanya ada n di akhir integer
+
+the "n" at the end means it's a BigInt
+const bigInt = 1234567890123456789012345678901234567890n;
+
+*/
+
+// ! PEMISAH
+
 // * STRINGS
 
 // * Strings itu data berupa teks
@@ -65,11 +143,140 @@ console.log(myNewLine, "<< Contoh output newLine")
 const newline = "One day you finally knew\nwhat you had to do, and began,";
 console.log(newline);
 
+// * Cara pakai tanda petik beneran di strings bagaimana?
+
+const exampleQuotes1 = `He said "I think this is fine"`
+const exampleQuotes2 = 'He said "I think this is fine"'
+
+// * Kalau mau menambah tipe data numbers + strings? Hasilnya seperti apa?
+
+const myString = 123
+const word = "Front "
+
+// Hasilnya pasti "Front 123"
+console.log(word + myString)
+
+// * NOTE: Penting belajar javascript string methods untuk menyelesaikan masalah (link w3schools ada dibawah)
+
+// ! PEMISAH
+
+// * BOOLEAN (LOGICAL TYPE)
+
+/* 
+
+Boolean type itu hanya punya dua values, antara true dan false
+
+tipe ini seringnya dipakai untuk values yang sifatnya true (artinya benar) atau false (artinya salah)
+
+misalnya
+
+let nameFieldChecked = true // ini tandanya name field sudah dicek karena true
+let ageFieldChecked = false // ini tandanya age field belum dicek karena false
+
+Boolean values juga sering digunakan untuk perbandingan/comparisons
+
+let isGreater = 4 > 1;
+
+alert(isGreater) hasilnya harusnya true karena 4 lebih besar dari 1
+
+*/
+
+// ! PEMISAH
+
+// * NULL
+
+/* 
+
+null value ini tidak dimiliki oleh type data apapun. Hanya membuat type tersendiri yang isinya hanya null
+
+seperti dokumentasi katakan null itu hanya special value yang merepresentasikan kosong, empty, nothing, value unknown.
+
+
+Contohnya
+
+let age = null
+
+maka age itu null
+
+*/
+
+// ! PEMISAH
+
+// * UNDEFINED VALUE
+
+/* 
+
+undefined value juga hampir sama dengan null karena dia bukan milik siapapun dan hanya tipe tersendiri
+
+arti undefined juga value is not assigned yang artinya variablenya ada tetapi tidak di assigned maka valuenya jadi undefined
+
+misalnya
+
+let age;
+
+alert(age) ini nanti hasilnya undefined
+
+*/
+
+// ! PEMISAH
+
+// * OBJECTS AND SYMBOLS
+
+/* 
+
+object type juga special, semua tipe data lain di panggil primitive kare value mereka hanya bisa menyimpan satu tipe data, sedangkan object bisa menyimpan koleksi data dan entitas yang lebih complex.
+
+sedangkan symbol type dibuat sebagai identitas unik untuk objects.
+
+*/
+
+// ! PEMISAH
+
+// * TYPEOF OPERATOR
+
+/* 
+
+typeof berguna ketika kita mau memproses suatu value yang memiliki tipe berbeda atau hanya ingin sekedar melakukan cek secara cepat.
+
+misalnya:
+
+typeof undefined // "undefined"
+
+typeof 0 // "number"
+
+typeof 10n // "bigint"
+
+typeof true // "boolean"
+
+typeof "foo" // "string"
+
+typeof Symbol("id") // "symbol"
+
+typeof Math // "object"  (1)
+
+typeof null // "object"  (2)
+
+typeof alert // "function"  (3)
+
+*/
+
 /* 
 
 Source belajar yang bagus
 
 https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Strings#numbers_vs._strings
+
+https://javascript.info/types
+
+https://www.petanikode.com/javascript-variabel/
+
+
+JAVASCRIPT STRING METHODS
+
+https://www.w3schools.com/js/js_string_methods.asp#mark_length
+
+https://www.w3schools.com/jsref/jsref_obj_string.asp
+
 
 
 */
