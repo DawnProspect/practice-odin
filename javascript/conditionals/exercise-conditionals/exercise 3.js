cfca4d7972bba9592421856e52afddcf60242cfa
DawnProspect <rzaljabar@gmail.com>
2025-03-22 15:48:17 +0700

    Add exercises folder in conditional

---
+++ javascript/conditionals/exercise-conditionals/exercise 3.js
@@ -0,0 +1,13 @@
+// const a = "one plus eight"
+// const b = "22 times three"
+// const c = "the *remainder* of 5/4"
+// const d = "the variable 'a' minus 17"
+// const e = "the sum of the previous four variables"
+
+
+const a = 1 + 8
+const b = 22 * 3
+const c = 5 % 4
+const d = a - 17
+// const e = [a, b, c, d].reduce((a, b) => a + b, 0)
+const e = a + b + c + d
---
+++ javascript/conditionals/exercise-conditionals/exercise1.js
@@ -0,0 +1,34 @@
+// * Exercise from OdinProject
+
+/**
+ * ===== Troubleshooting =====
+ * The function below should log the number 2, however it does not, 
+ * see if you can fix it!
+ * Be sure to fix it in the spirit of the code, do not hard code the result.
+ */
+
+function troubleshooting() {
+	const a = 1;
+	const b = 1;
+
+	let result;
+
+	// Edit between these lines
+	// =================================
+	// result = "a" + "b";
+
+	// Diganti harusnya menjadi
+
+    // Jawabanku begini dan ini benar
+
+	result = a + b
+
+	console.log(result, "<< Angka result")
+
+	// =================================
+
+	return result;
+}
+
+// Do not change this 
+module.exports = troubleshooting;
